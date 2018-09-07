fbc6eb9de0135f25b9aeaa30f2fe86d5607d20a8
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-09-07 08:19:25 +0300

    Implement data

---
+++ MeTube/MeTube.Data/MeTubeContext.cs
@@ -0,0 +1,18 @@
+﻿namespace MeTube.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class MeTubeContext : DbContext
+    {
+        public MeTubeContext()
+        {
+        }
+
+        public MeTubeContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Tube> Tubes { get; set; }
+    }
+}
