e564a20760c409c0e22e03da57750f42de48d1aa
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-09-10 16:14:51 +0300

    Create upload page

---
+++ MeTube/MeTube/Models/BindingModels/TubeUploadBindingModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MeTube.Models.BindingModels
+{
+    public class TubeUploadBindingModel
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string YouTubeLink { get; set; }
+
+        public string Description { get; set; }
+    }
+}
